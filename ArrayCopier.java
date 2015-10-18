7134b82aa8bd3726f2c63f715aa4760ea0955aae
unknown <pdomok01@CSPC110.dcs.bbk.ac.uk>
2015-10-18 17:27:42 +0100

    it now works after some changes

--- ArrayCopier.java
+++ ArrayCopier.java
@@ -1,15 +1,23 @@
+//solved some basic errors
+
 public class ArrayCopier
 {
-	public int[] copyArray( int[] src , int[] dst )
+	public static int[] copyArray( int[] src , int[] dst )
 	{
-		if (dst.length() <= src.length())
-			for (int i = 0 ; i = dst.length() - 1; arrayPos++)
-				dst[arrayPos]  = src[arrayPos];
-		else
-			for (int i = 0 ; i = src.length() - 1; arrayPos++)
-				dst[arrayPos]  = src[arrayPos];
-			for (int arrayPos = src.length; arrayPos = dst.length()-1; arrayPos++)
-				dst[arrayPos] = 0;
+		if (dst.length <= src.length)                 
+        // error solved- couldn't find symbol dst.length() because there should not be () after length.
+		{
+			for (int i = 0 ; i <= dst.length - 1; i++)
+				dst[i]  = src[i];
+		}
+		else 
+		{
+			for (int i = 0 ; i <= src.length - 1; i++)
+				dst[i]  = src[i];
+			for (int i = src.length; i <= dst.length-1; i++)
+				dst[i] = 0;
+		}
+		return dst;
 	}
 }
 			
--- CopyAnArray.java
+++ CopyAnArray.java
@@ -1,27 +1,45 @@
-java.util.Scanner;
+//made a new helper method called arrayAsString which turns the array
+//into a string so I can tell the user what the values in their arrays are.
+//This is usually called a 'toString' method, but normally it would be in a separate class called Array
+//I am going to ask if it is better to do the array as a separate class with a toString method
+
+import java.util.Scanner;
 
 public class CopyAnArray
 {
 	public static void main(String[] args)
 	{
 	int[] firstArray = makeArray();
+	System.out.println("The first array is: " + arrayAsString(firstArray));
 	int[] secondArray = makeArray();
+	System.out.println("The second array is: " + arrayAsString(secondArray));
 	ArrayCopier.copyArray(firstArray , secondArray);
+	System.out.println ("The second array is now: " + arrayAsString(secondArray));
 	}
 	
 	
 	private static int[] makeArray()
 	{
-		println ("Enter the size of your next array")
-		int arraySize = scanner.nextInt()
-		int[] thisArray = new int[arraySize];}
+		Scanner scanner = new Scanner(System.in);
+		System.out.println ("Enter the size of your next array: ");
+		int arraySize = scanner.nextInt();
+		int[] thisArray = new int[arraySize];
 		int pos = 1;
 		for (int i = 1 ; i <=arraySize; i++)
 		{
-			println (Enter the number for the " + pos + " position: ")
+			System.out.println ("Enter the number for position " + pos + ": ");
 			thisArray[i-1] = scanner.nextInt();
 			pos++;
 		}
 		return thisArray;
+	}
+	
+	private static String arrayAsString(int[] thisArray)
+	{
+		String arrayAsString = "( ";
+		for (int i = 0; i<= thisArray.length -1 ; i++)
+			arrayAsString += thisArray[i] + " , ";
+		arrayAsString += ")";
+		return arrayAsString;
 	}
 }
